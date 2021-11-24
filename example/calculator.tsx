f8dedfe095e4258a3fe637ca510c350a665c018a
Filip Paulů <ing.fenix@seznam.cz>
2021-11-24 17:04:12 +0100

    Update formating values in calculator

--- example/calculator.tsx
+++ example/calculator.tsx
@@ -9,26 +9,31 @@ function Calculator() {
     const [ operation, setOperation ] = useState("");
     const [ numberBefore, setNumberBefore ] = useState(0);
 
+    const value = Number(number);
+
+    function toString(value: number): string {
+        return value.toString();
+    }
+
     function addNumber(num: number) {
-        setNumber(n => Number(n + num).toString());
+        setNumber(n => toString(Number(n + num)));
     }
 
     function addOperation(operation: string) {
-        setNumberBefore(Number(number));
-        setNumber("0");
+        setNumberBefore(value);
         setOperation(operation);
+        setNumber("0");
     }
 
     function count() {
-        const value = Number(number);
         setNumberBefore(value);
 
         switch (operation) {
-            case "%": return setNumber(String(numberBefore * value / 100));
-            case "/": return setNumber(String(numberBefore / value));
-            case "*": return setNumber(String(numberBefore * value));
-            case "-": return setNumber(String(numberBefore - value));
-            case "+": return setNumber(String(numberBefore + value));
+            case "%": return setNumber(toString(numberBefore * value / 100));
+            case "/": return setNumber(toString(numberBefore / value));
+            case "*": return setNumber(toString(numberBefore * value));
+            case "-": return setNumber(toString(numberBefore - value));
+            case "+": return setNumber(toString(numberBefore + value));
         }
     }
 
@@ -43,7 +48,7 @@ function Calculator() {
                 <Button color="#444" onPress={() => setNumber(n => String(-Number(n)))}>+/-</Button>
                 <Button color="#444" onPress={() => addOperation("%")}>%</Button>
                 <Button color="orange" onPress={() => addOperation("/")}>/</Button>
-                
+
                 <Button onPress={() => addNumber(7)}>7</Button>
                 <Button onPress={() => addNumber(8)}>8</Button>
                 <Button onPress={() => addNumber(9)}>9</Button>
