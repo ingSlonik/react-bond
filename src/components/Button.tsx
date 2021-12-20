e505a851a4841092163b2514dc9e08e71e80e7c9
Filip Paulů <ing.fenix@seznam.cz>
2021-12-20 23:57:15 +0100

    Add button element

---
+++ src/components/Button.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Pressable } from "./Pressable";
+import { Text } from "./Text";
+
+export type ButtonProps = {
+    title: string,​
+    onPress: () => void,
+    // accessibilityLabel
+    // accessibilityActions
+    // onAccessibilityAction
+    color: string,
+    disabled: boolean,
+    // hasTVPreferredFocus
+    // nextFocusDown
+    // nextFocusForward
+    // nextFocusLeft
+    // nextFocusRight
+    // nextFocusUp
+    // testID
+    // touchSoundDisabled
+};
+
+Button.defaultProps = {
+    color: "#F1F1F1",
+    disabled: false,
+};
+
+export function Button({ onPress, title, color, disabled }: ButtonProps): JSX.Element {
+    return <Pressable
+        style={{ 
+            borderRadius: 4,
+            borderColor: "#888888",
+            borderWidth: 1,
+            borderStyle: "solid",
+            paddingBottom: 4,
+            paddingTop: 4,
+            paddingLeft: 16,
+            paddingRight: 16,
+            backgroundColor: color,
+            opacity: disabled ? 0.6 : 1,
+        }}
+        onPress={disabled ? () => {} : onPress}
+    >
+        <Text style={{ textAlign: "center" }}>{title}</Text>
+    </Pressable>
+}
