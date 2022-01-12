8de88e5c70e618a826ff6d2724ab6af0b7892dfa
Filip Paulů <ing.fenix@seznam.cz>
2022-01-12 23:25:34 +0100

    Export Image component

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { Window, WindowProps } from "./components/Window";
 export { View, ViewProps } from "./components/View";
 export { Text, TextProps } from "./components/Text";
 export { Pressable, PressableProps } from "./components/Pressable";
+export { Image, ImageProps } from "./components/Image";
 
 // combined components
 export { Button, ButtonProps } from "./components/Button";
