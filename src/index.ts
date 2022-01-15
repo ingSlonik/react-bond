cbe2cbc679e39e8461859929ee89abeb2b0c20fe
Filip Paulů <ing.fenix@seznam.cz>
2022-01-15 14:06:31 +0100

    Export file component

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export { View, ViewProps } from "./components/View";
 export { Text, TextProps } from "./components/Text";
 export { Pressable, PressableProps } from "./components/Pressable";
 export { Image, ImageProps } from "./components/Image";
+export { File, FileProps } from "./components/File";
 export { Input } from "./components/Input";
 
 // combined components
