71367df81c523ea79fdd64775d928c129756d884
Filip Paulů <ing.fenix@seznam.cz>
2022-01-15 14:30:10 +0100

    Update file example

--- example/file.tsx
+++ example/file.tsx
@@ -6,11 +6,13 @@ import { render, Window, View, Text, File } from "../src";
 function App() {
     const [paths, setPaths] = useState<string[]>([]);
 
-    return <Window title="Upload file example" width={420} height={250}>
+    return <Window title="Upload file example" width={640} height={250}>
         <View style={{ flexGrow: 1, justifyContent: "flex-start", padding: 32 }}>
             <File onFiles={setPaths} />
 
-            {paths.map(path => <Text key={path}>{path}</Text>)}
+            {paths.map(path => <View key={path} style={{ marginTop: 8 }}>
+                <Text>{path}</Text>
+            </View>)}
         </View>
     </Window>;
 }
