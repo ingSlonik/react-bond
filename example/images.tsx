3022964847c5064550fcdca7e499b17c09691125
Filip Paulů <ing.fenix@seznam.cz>
2022-01-15 12:09:52 +0100

    Add relative path for images to examples

--- example/images.tsx
+++ example/images.tsx
@@ -3,12 +3,15 @@ import { resolve } from "path";
 import React from "react";
 import { render, Window, View, Text, Image } from "../src";
 
-render(<Window title="Images app" width={640} height={480}>
+render(<Window title="Images app" width={640} height={660}>
     <View style={{ flexGrow: 1, alignItems: "flex-start", padding: 16 }}>
         <H1>Images</H1>
 
         <H2>Image from url</H2>
         <Image src="https://picsum.photos/128" />
+
+        <H2>Image from file (relative path from root)</H2>
+        <Image src="./example/turtle.png" />
 
         <H2>Image from file (absolute path)</H2>
         <Image src={resolve(__dirname, "turtle.png")} />
