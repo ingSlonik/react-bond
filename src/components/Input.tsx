2f115851f9ae676072299978f0684a798db203aa
Filip Paulů <ing.fenix@seznam.cz>
2022-01-19 15:23:42 +0100

    Add disable and maxLength to Inputs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,40 +2,43 @@ import React from "react";
 import { getCSSProperties } from "../instance";
 import { TextStyle, LayoutStyle } from "../types";
 
-type Style = Partial<TextStyle & LayoutStyle>;
+type GeneralInputProps = {
+    style?: Partial<TextStyle & LayoutStyle>,
+    disable?: boolean,
+};
 
 export const Input = {
-    Text({ style, multiline, value, onChange }: { style?: Style, multiline?: boolean, value: string, onChange: (value: string) => void }) {
+    Text({ style, value, multiline, onChange, ...props }: GeneralInputProps & { multiline?: boolean, maxLength?: number, value: string, onChange: (value: string) => void }) {
         const cssProperties = getCSSProperties(style);
 
         if (multiline) {
-            return <textarea style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
+            return <textarea {...props} style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
         } else {
-            return <input type="text" style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
+            return <input {...props} type="text" style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
         }
     },
-    Password({ style, value, onChange }: { style?: Style, value: string, onChange: (value: string) => void }) {
+    Password({ style, value, onChange, ...props }: GeneralInputProps & { value: string, maxLength?: number, onChange: (value: string) => void }) {
         const cssProperties = getCSSProperties(style);
-        return <input type="password" style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
+        return <input {...props} type="password" style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
     },
-    Color({ style, value, onChange }: { style?: Style, value: string, onChange: (value: string) => void }) {
+    Color({ style, value, onChange, ...props }: GeneralInputProps & { value: string, onChange: (value: string) => void }) {
         const cssProperties = getCSSProperties(style);
-        return <input type="color" style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
+        return <input {...props} type="color" style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
     },
-    Date({ style, value, onChange }: { style?: Style, value: string, onChange: (value: string) => void }) {
+    Date({ style, value, onChange, ...props }: GeneralInputProps & { value: string, onChange: (value: string) => void }) {
         const cssProperties = getCSSProperties(style);
-        return <input type="date" style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
+        return <input {...props} type="date" style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
     },
-    Time({ style, value, onChange }: { style?: Style, value: string, onChange: (value: string) => void }) {
+    Time({ style, value, onChange, ...props }: GeneralInputProps & { value: string, onChange: (value: string) => void }) {
         const cssProperties = getCSSProperties(style);
-        return <input type="time" style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
+        return <input {...props} type="time" style={cssProperties} value={value} onChange={e => onChange(e.target.value)} />;
     },
-    Number({ style, value, min, max, onChange }: { style?: Style, value: number, min?: number, max?: number, onChange: (value: number) => void }) {
+    Number({ style, value, min, max, onChange, ...props }: GeneralInputProps & { value: number, min?: number, max?: number, onChange: (value: number) => void }) {
         const cssProperties = getCSSProperties(style);
-        return <input type="number" style={cssProperties} value={value} min={min} max={max} onChange={e => onChange(Number(e.target.value))} />;
+        return <input {...props} type="number" style={cssProperties} value={value} min={min} max={max} onChange={e => onChange(Number(e.target.value))} />;
     },
-    Range({ style, value, step, min, max, onChange }: { style?: Style, value: number, step?: number, min?: number, max?: number, onChange: (value: number) => void }) {
+    Range({ style, value, step, min, max, onChange, ...props }: GeneralInputProps & { value: number, step?: number, min?: number, max?: number, onChange: (value: number) => void }) {
         const cssProperties = getCSSProperties(style);
-        return <input type="range" style={cssProperties} value={value} step={step} min={min} max={max} onChange={e => onChange(Number(e.target.value))} />;
+        return <input {...props} type="range" style={cssProperties} value={value} step={step} min={min} max={max} onChange={e => onChange(Number(e.target.value))} />;
     },
 };
