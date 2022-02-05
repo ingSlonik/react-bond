6ad4aad9dbe81caa99b0a7d5f754f0e5bd479845
Filip Paulů <ing.fenix@seznam.cz>
2022-02-05 23:07:27 +0100

    Update hot reloading and add hot keys

--- src/scripts/hot.ts
+++ src/scripts/hot.ts
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 
 import Module from "module";
-import { resolve } from "path";
+import { resolve, relative } from "path";
 import { existsSync, readFileSync, watchFile, unwatchFile, lstatSync } from "fs";
 
 import { useState } from "react";
 
+const cwd = process.cwd();
 const watchedFiles: string[] = [];
 const requireExtensions = [".js", ".ts", ".tsx", ".json"];
 
 const entryPath = process.argv[2];
 if (!entryPath) throw new Error("There is not set entry file as first command argument.");
 
-const absoluteEntryPath = resolve(process.cwd(), entryPath);
+const absoluteEntryPath = resolve(cwd, entryPath);
 if (!existsSync(absoluteEntryPath)) throw new Error(`Entry file "${absoluteEntryPath}" doesn't exist.`);
 
 console.log("✓ react-bond hot reloading is turned on.");
@@ -22,7 +23,7 @@ global._reactBondEntryPath = absoluteEntryPath;
 global._reactBondGetHotRequire = hotReloadRequire;
 
 let typeScriptConfig = null;
-const tsconfigPath = resolve(process.cwd(), "tsconfig.json");
+const tsconfigPath = resolve(cwd, "tsconfig.json");
 if (existsSync(tsconfigPath)) {
     const { readConfigFile } = require("typescript");
     const options = readConfigFile(tsconfigPath, (path: string) => readFileSync(path, "utf8"));
@@ -35,18 +36,57 @@ if (typeScriptConfig) {
     console.log("- Without TypeScript compilation.");
 }
 
-Module.wrap = (code) => {
-    return `(function (exports, require, module, __filename, __dirname) {
-    require = global._reactBondGetHotRequire(require, __dirname);
-    ${compile(code)}
-    });`;
-};
+Module.wrap = (code) => `(function (exports, require, module, __filename, __dirname) {
+require = global._reactBondGetHotRequire(require, __dirname);
+${compile(code)}
+});`;
 
-// RUN
+startStdin();
+console.log(`✓ Hot keys activated (R and ${process.platform === "darwin" ? "CMD" : "CTRL"}+C).`);
+
+// -------------- run --------------
 console.log("");
-require(absoluteEntryPath);
+console.log(`Staring application...`);
+try {
+    require(absoluteEntryPath);
+} catch (e) {
+    throw e;
+}
 
 watch(absoluteEntryPath, () => console.log("Entry file was changed, whole application is reloading."));
+
+// -------------- functions --------------
+
+function startStdin() {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.resume();
+    stdin.setEncoding("utf8");
+    stdin.on("data", stdinData);
+}
+
+function stopStdin() {
+    const stdin = process.stdin;
+    stdin.off("data", stdinData);
+    process.exit();
+}
+
+function stdinData(buffer: Buffer) {
+    const key = buffer.toString();
+
+    if (key === '\u0003') {
+        console.log(`${process.platform === "darwin" ? "CMD" : "CTRL"} + C - close react-bond hot reloading.`);
+        process.exit();
+    } else if (key === "r") {
+        console.log("R - reloading application.");
+        watchedFiles.forEach(file => {
+            delete require.cache[require.resolve(file)];
+        });
+        require(absoluteEntryPath);
+    } else {
+        console.log(`${key} - no effect. Only R for reload and ${process.platform === "darwin" ? "CMD" : "CTRL"}+C for close is allowed.`);
+    }
+}
 
 function compile(source: string): string {
     if (typeScriptConfig) {
@@ -61,31 +101,36 @@ function hotReloadRequire(originalRequire: NodeRequire, __dirname: string): Node
     const require = (path: string): any => {
         if (path.startsWith(".") || path.startsWith("/")) {
 
-            const fullPath = getPathWithExtension(resolve(path.startsWith(".") ? __dirname : process.cwd(), path));
+            const fullPath = getPathWithExtension(resolve(path.startsWith(".") ? __dirname : cwd, path));
             if (!fullPath.includes("node_modules")) {
                 const originalModule = originalRequire(fullPath);
 
-                const hotComponents = {};
-                Object.entries(originalModule).forEach(([name, component]) => {
-                    if (typeof component === "function" && (name === "default" || startsWithCapital(name))) {
-                        // can by react component
-                        const hotComponent = hot(component);
-                        hotComponents[name] = hotComponent;
-                        // originalModule[name] = hotComponent;
+                const hotFunctions = {};
+                Object.entries(originalModule).forEach(([name, fn]) => {
+                    if (typeof fn === "function") {
+                        if (name === "default" || startsWithCapital(name)) {
+                            // can by react component
+                            const hotComponent = hot(fn, true);
+                            hotFunctions[name] = hotComponent;
+                        } else {
+                            // all exported functions
+                            const hotFunction = hot(fn, false);
+                            hotFunctions[name] = hotFunction;
+                        }
                     }
                 });
 
-                if (Object.keys(hotComponents).length > 0) {
+                if (Object.keys(hotFunctions).length > 0) {
                     watch(fullPath, (newModule) => {
-                        for (const name in hotComponents) {
-                            hotComponents[name]._reactBondHotUpdate(newModule[name]);
+                        for (const name in hotFunctions) {
+                            hotFunctions[name]._reactBondHotUpdate(newModule[name]);
                         }
                     });
                 }
 
                 return {
                     ...originalModule,
-                    ...hotComponents,
+                    ...hotFunctions,
                 };
             } else {
                 return originalRequire(fullPath);
@@ -108,7 +153,7 @@ function hotReloadRequire(originalRequire: NodeRequire, __dirname: string): Node
 
 function watch(path: string, callback: (module: any) => void) {
     if (!watchedFiles.includes(path)) {
-        console.log("Watch file:", path);
+        console.log("Watch:", relative(cwd, path));
         watchedFiles.push(path);
 
         watchFile(path, { interval: 500 }, () => {
@@ -122,17 +167,21 @@ function watch(path: string, callback: (module: any) => void) {
 
 function unwatch() {
     watchedFiles.forEach(file => {
-        console.log("Unwatch file:", file);
+        console.log("Unwatch:", relative(cwd, file));
         unwatchFile(file);
     });
+
+    stopStdin();
 }
 
-function hot(component: Function): Function {
+function hot(component: Function, isComponent: boolean): Function {
     const hotComponent = function () {
         const component = hotComponent._reactBondHotComponent;
 
-        const [_, setReload] = useState({});
-        hotComponent._reactBondHotReload = () => setReload({});
+        if (isComponent) {
+            const [_, setReload] = useState({});
+            hotComponent._reactBondHotReload = () => setReload({});
+        }
 
         return component.apply(this, arguments);
     };
